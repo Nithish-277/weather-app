28c9067ed64605f2e22f6dfc4ec255ac59fd9aff
Nithish-277 <44161030+Nithish-277@users.noreply.github.com>
2021-12-27 22:51:28 +0530

    Added content to forecast string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude,longitude,callback) => {
         } else if(response.body.error){
             callback('Unable to find location to provide forecast')
         } else{
-            callback(undefined,response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.')
+            callback(undefined,response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out. The humidity is ' + response.body.current.humidity + '%')
         }
     })
 }
